639b51c8f902b60620604f328f3d1863dfadcb45
liushaoxiong <3354621952@qq.com>
2022-10-27 17:38:02 +0800

    主方法提交

--- main.go
+++ main.go
@@ -7,13 +7,22 @@ package main
  */
 
 import (
+	"djwkYbParse/entity"
 	"djwkYbParse/service"
-	"github.com/gin-gonic/gin"
+	"encoding/json"
+	"fmt"
+	"strconv"
 )
 
 func main() {
-	//db.CreateTableGrom(entity.ShopYbInfo{})
-	r := gin.Default()
-	r.POST("/ybShopInsert", service.JsonRequestBody)
-	r.Run(":8300")
+	//r := gin.Default()
+	//r.POST("/ybShopInsert", service.JsonRequestBody)
+	//r.Run(":8300")
+	url := "https://code.nhsa.gov.cn/yp/stdGoodsPublic/getStdGoodsPublicData.html"
+	method := "POST"
+	text := "goodsCode=&companyNameSc=&registeredProductName=&approvalCode=&_search=false&nd=&rows=" + strconv.Itoa(20) + "&page=" + strconv.Itoa(10) + "&sidx=&sord=asc"
+	info, _ := service.RequestMedicineInfo(url, text, method, entity.MedicineResponse1001{})
+	medicine1003 := entity.MedicineResponse1003{}
+	json.Unmarshal(info, &medicine1003)
+	fmt.Println(medicine1003)
 }
